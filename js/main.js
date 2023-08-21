6efe7216f932974b39b9f017bc0ecc9f6c7f6bcb
Derin DeNio <derindenio@gmail.com>
2023-08-21 17:43:20 -0600

    added functionality to pull which subclasses can use inputed spell, some design additions

--- js/main.js
+++ js/main.js
@@ -2,13 +2,24 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
-
+    //clear our ul each button click for new spells
+    document.querySelector('ul').innerHTML = '';
+    const choice = document.querySelector('input').value
+    const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+    
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       
+        console.log(data)
+        data.subclasses.forEach( obj => {
+            console.log(obj.name);
+            //create an li
+            const li = document.createElement('li');
+            //add text to li
+            li.textContent = obj.name;
+            //append li to ul
+            document.querySelector('ul').appendChild(li);
+        })
       })
       .catch(err => {
           console.log(`error ${err}`)
